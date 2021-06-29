112f0d17d9ed583677c3b1d28a290f684bae0cfe
popofzk <847473488@qq.com>
2021-06-29 10:42:54 +0800

    offlineMapSpringboot

---
+++ src/main/java/com/public_data/public_data/OfflinemapApplication.java
@@ -0,0 +1,13 @@
+package com.public_data.public_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflinemapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfflinemapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/public_data/public_data/OfflinemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.public_data.public_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflinemapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
